67dd81bbe0ac69e07c7958f538b7d19a68db4b0e
Porto <porto.ing88@gmail.com>
2019-01-15 22:25:55 -0300

    ajuste

--- handler.js
+++ handler.js
@@ -1,7 +1,10 @@
 'use strict';
 
 module.exports.hello = async (event, context) => {
-  return "luis";
+  return {
+    statusCode: 200,
+    body: 'luis',
+  };
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
